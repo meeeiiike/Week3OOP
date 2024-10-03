12f473fbce8716bc3f695746975d5d3b60c7c4a5
meeeiiike <g00407095@atu.ie>
2024-10-03 18:43:38 +0100

    removed exp for demo

--- src/main/java/ie/atu/week3/Calculator.java
+++ src/main/java/ie/atu/week3/Calculator.java
@@ -28,11 +28,6 @@ public class Calculator {
         System.out.println("Answer: " + sum);
     }
 
-    public static void exp(int firstNum, int secondNum) {
-        double sum = Math.pow(firstNum, secondNum);
-        System.out.println("Answer: " + sum);
-    }
-
     /**
      *  | Main Method |
      * Prompt user for two numbers.
@@ -69,9 +64,6 @@ public class Calculator {
                     } else {
                         divide(firstNum, secondNum);
                     }
-                    break;
-                case "exp":
-                    exp(firstNum, secondNum);
                     break;
                 default:
                     System.out.println("Invalid operation");
